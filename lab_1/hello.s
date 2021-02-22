21b6c0281bf7045d3ac9c8163663dba0d426496d
Felix Luebken <felixluebken2000@gmail.com>
2021-02-22 18:21:31 +0100

    lab 1 done

---
+++ lab_1/hello.s
@@ -0,0 +1,26 @@
+#************************************************************************
+#*  PROGRAM NAME: Hello World						*
+#*  DESCRIPTION:  Your standard hello world program in x86-64 assembly.	*
+#*  AUTHORS: 	  Felix Luebken & Taco Timmers				*
+#************************************************************************
+
+.text
+str:	.asciz	"Felix Luebken, Taco Timmers\nstudy numbers...\nAssignment 1: your first simple program.\n"
+
+.global main
+#************************************************************************
+#*  SUBROUTINE:   main							*
+#*  DESCRIPTION:  prints $str and exits 				*
+#************************************************************************
+main:
+	pushq	%rbp			#store the caller's base pointer
+	movq	%rsp, %rbp		#initialize the base pointer
+	
+	movq	$0, %rax		#no vector registers for printf
+	movq	$str, %rdi		#load string str
+	call	printf			#call printf subroutine
+	jmp	end			#call end subroutine
+
+end:
+	mov	$0, %rdi		#loads exit code
+	call	exit			#exits the program
